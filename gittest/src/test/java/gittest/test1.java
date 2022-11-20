0d3855c2d45d6f26f6f8ec4e40fb026b9093f335
LENOVO <LENOVO@DESKTOP-6F1UH3N>
2022-11-20 11:34:52 +0530

    this is first commit

---
+++ gittest/src/test/java/gittest/test1.java
@@ -0,0 +1,12 @@
+package gittest;
+
+public class test1 {
+	
+	public static void main(String[] args) {
+		
+		
+		System.out.println("this is kartik first");
+		System.out.println("it is not easy pr");
+	}
+
+}
